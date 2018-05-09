b0ad388f409986134d3177e99c09a42876fb3ee9
camander321 <chamburg321@gmail.com>
2018-05-08 23:04:47 -0700

    add unity example csproj to solution

---
+++ SmartRacer/Assets/Scripts/TrackGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackGenerator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("this is a test");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
